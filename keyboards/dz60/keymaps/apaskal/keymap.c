ea0138fe90de32a7f0c5b61d497938f21a25e8f5
Andrey Paskal <apaskal@gmail.com>
2017-11-14 16:10:29 +0300

    Added Ins key

--- keyboards/dz60/keymaps/apaskal/keymap.c
+++ keyboards/dz60/keymaps/apaskal/keymap.c
@@ -10,7 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC, 
 		F(0),    KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, F(1), 
 		KC_LSFT, KC_NO,KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_DEL, 
-		KC_NO,   KC_LGUI, KC_LALT, MO(2), KC_SPC, MO(2), KC_RALT, KC_RCTL , KC_NO, MO(3), KC_NO),
+		KC_NO,   KC_LGUI, KC_LALT, MO(2), KC_SPC, MO(2), KC_RALT, KC_RCTL , KC_NO, LT(3,KC_INS), KC_NO),
 
 // mac os layout
 	KEYMAP(
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC, 
 		F(0),    KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, F(1), 
 		KC_LSFT, KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_DEL, 
-		KC_NO,   KC_LALT, KC_LGUI, MO(2), KC_SPC, MO(2), KC_RALT, KC_RCTL , KC_NO, MO(3), KC_NO),
+		KC_NO,   KC_LALT, KC_LGUI, MO(2), KC_SPC, MO(2), KC_RALT, KC_RCTL , KC_NO, LT(3,KC_INS), KC_NO),
 
 // navigation and keypad layout
 	KEYMAP(
